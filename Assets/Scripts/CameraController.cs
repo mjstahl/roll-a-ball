bbd77c4d47f560c316b3fd11cd17be34d95a5dae
Mark Stahl <mark@work.local>
2015-01-04 17:09:29 -0500

    Initial commit - step 4 completed

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position;
+	}
+	
+	// Follow Cameras, Procedural Animation, Get Last Known States
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
